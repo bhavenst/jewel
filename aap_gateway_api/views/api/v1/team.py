8dfecfb52810828b83ed8571316236d6bfb3e4d7
Alan Rominger <arominge@redhat.com>
2025-09-03 16:35:24 -0400

    AAP-52186 Check MANAGE_ORGANIZATION_AUTH on team creation, for 400 error (#972)
    * Check MANAGE_ORGANIZATION_AUTH on team creation, for 400 error
    
    Add Special condition for superuser that allows them to manage organizations despite the value of the recently created MANAGE_ORGANIZATION_AUTH dynamic preference.

--- aap_gateway_api/views/api/v1/team.py
+++ aap_gateway_api/views/api/v1/team.py
@@ -1,5 +1,10 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import status
+from rest_framework.response import Response
+
 from aap_gateway_api.models import Team
 from aap_gateway_api.serializers import TeamSerializer
+from aap_gateway_api.utils.preferences import get_preference_value
 from aap_gateway_api.views.api.v1.common import ResourceAPIUpdateMixin, RoleModelViewSet
 
 
@@ -10,3 +15,9 @@ class TeamViewSet(ResourceAPIUpdateMixin, RoleModelViewSet):
 
     queryset = Team.objects.select_related("resource").all()
     serializer_class = TeamSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Check if organization management is enabled, but allow superusers to bypass this restriction
+        if not get_preference_value('configuration', 'MANAGE_ORGANIZATION_AUTH') and not request.user.is_superuser:
+            return Response({"detail": _("Team creation is disabled when MANAGE_ORGANIZATION_AUTH is false.")}, status=status.HTTP_400_BAD_REQUEST)
+        return super().create(request, *args, **kwargs)
